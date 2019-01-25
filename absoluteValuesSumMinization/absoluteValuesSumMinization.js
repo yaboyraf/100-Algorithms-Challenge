2bfbc2e0df88900fb4fa032f6fd2719a65c3ab80
Rafael Guevara <mmmsnacks@gmail.com>
2019-01-24 21:50:41 -0800

    different solution

--- absoluteValuesSumMinization/absoluteValuesSumMinization.js
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.js
@@ -1,13 +1,11 @@
 function absoluteValuesSumMinimization(array){
-    console.log(Math.floor(array.length / 2));
-    let index = Math.floor(array.length / 2);
-    console.log('index is: ', index);
+    //variable to check if array has even or odd index
     const isEven = array.length % 2 === 0;
-    if (isEven) {
-        return array[index - 1];
-    } else {
-        return array[index];
-    }
+
+    console.log(isEven);
+    //console.log(array[Math.floor(array.length / 2)]);
+    
+    return isEven ? array[array.length / 2 - 1] : array[Math.floor(array.length / 2)];
 }
 
 
